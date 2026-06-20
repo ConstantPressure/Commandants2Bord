9afba6d7502e3040824d9443a588b01360edf917
Léo Barbier <leo.barbier@epitech.eu>
2026-06-20 16:46:56 +0200

    feat: lever

--- Scripts/Submarin/motor.gd
+++ Scripts/Submarin/motor.gd
@@ -6,21 +6,6 @@ var speed := 0.0
 var target_speed := 0.0
 
 @export var max_speed := 300                        
-@export var speed_update_speed := 0.2
-
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("test_action1"):
-		target_speed = 1.0
-	elif Input.is_action_pressed("test_action4"):
-		target_speed = -1.0
-	else:
-		target_speed = 0.0
-	
-	speed = lerp(
-		speed,
-		target_speed,
-		speed_update_speed * delta
-	)
-	
-	submarin.speed = speed * max_speed
+	submarin.speed = $Lever.lever_rotation * max_speed
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,16 +4,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var controlled_utility = null
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -22,4 +20,24 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	if not controlled_utility:
+		move_and_slide()
+	
+func _input(event: InputEvent) -> void:
+	if controlled_utility:
+		controlled_utility._input(event)
+		
+		if event.is_action("interact") and event.is_pressed():
+			if controlled_utility.has_method("exit"):
+				controlled_utility.exit()
+			controlled_utility = null
+		
+		return
+	
+	if event.is_action("interact") and event.is_pressed():
+		controlled_utility = $UtilityDetector.get_closest()
+	
+	if event is InputEventKey:
+		if event.pressed and event.is_action("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+	
