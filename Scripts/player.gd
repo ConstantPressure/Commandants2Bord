b666dc49aa37055616a5f8f501e96bc3460e359f
JulesDELTEIL <jules.delteil@epitech.eu>
2026-06-20 16:44:36 +0200

    feat(submarin): background for inside of the submarin

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
+@onready var anim: AnimatedSprite2D = $"AnimatedSprite2D"
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _ready() -> void:
+	anim.play("idle")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,8 +21,10 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
+		anim.play("move")
 		velocity.x = direction * SPEED
 	else:
+		anim.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
