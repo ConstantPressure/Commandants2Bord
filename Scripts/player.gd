b0861c4f617ff7f027c7c1e844373125969c426a
Léo Barbier <leo.barbier@epitech.eu>
2026-06-21 20:21:01 +0200

    feat: jetpack

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,9 +4,11 @@ extends CharacterBody2D
 @onready var submarin: Node2D = get_parent()
 
 const SPEED = 300.0
+const FLY_SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 var player_id: int = 1
 var prefix: String
+var is_flying: bool = false
 var controlled_utility = null
 
 func _ready() -> void:
@@ -49,18 +51,36 @@ func handle_interractable(event):
 				print(submarin.is_ligth_on)
 
 func move(delta):
-	if not is_on_floor():
+	if not is_on_floor() and not is_flying:
 		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed(prefix + "jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed(prefix + "fly"):
+		is_flying = not is_flying
 
-	var direction := Input.get_axis(prefix + "left", prefix + "right")
-	if direction:
-		anim.play("move")
-		velocity.x = direction * SPEED
-		anim.flip_h = direction < 0
+	if not is_flying:
+		var direction := Input.get_axis(prefix + "left", prefix + "right")
+		if direction:
+			anim.play("move")
+			velocity.x = direction * SPEED
+			anim.flip_h = direction < 0
+		else:
+			anim.play("idle")
+			velocity.x = 0
 	else:
-		anim.play("idle")
-		velocity.x = 0
+		var direction := Input.get_axis(prefix + "left", prefix + "right")
+		
+		if direction:
+			anim.play("move")
+			velocity.x = direction * FLY_SPEED
+			anim.flip_h = direction < 0
+		else:
+			anim.play("idle")
+			velocity.x = 0
+	
+		var direction_y := Input.get_axis(prefix + "up", prefix + "down")
+		velocity.y = direction_y * FLY_SPEED
+	
 	move_and_slide()
