8a31c30eb51a4f71f08d96501104af87a8abe935
sami <sami12022005@yahoo.fr>
2026-06-21 17:02:21 +0200

    feat: water level rising when ship has taken damages

--- Scripts/Submarin/submarin.gd
+++ Scripts/Submarin/submarin.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
+@onready var water_shader: Polygon2D = $Boundaries/InsideBackground/Polygon2D
 
 var direction: Vector2 = Vector2(1.0, 0.0)
 var speed: float = 0.0
 
 var health: float = 100
+
 var is_ligth_on: bool = true
 var knockback: Vector2 = Vector2.ZERO
 
@@ -27,6 +29,9 @@ func _physics_process(_delta: float) -> void:
 	var motor_velocity = (Vector2.RIGHT * speed).rotated(rotation)
 	velocity = motor_velocity + knockback
 	knockback = knockback.lerp(Vector2.ZERO, KNOCKBACK_DECAY)
+	var water_level = lerp(0.697, 0.659, health / 100.0)
+	print(water_level)
+	water_shader.material.set("shader_parameter/liquid_level", water_level)
 	move_and_slide()
 
 func _process(_delta: float) -> void:
